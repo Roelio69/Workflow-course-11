47c85a2401199534f9a3ef6fcdbd87b84a9c1b08
Roel_Beumers <roel_beumers@hotmail.com>
2014-05-30 01:49:10 +0200

    toevoegen MEME script

---
+++ callMEME.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+class class4():
+    def callMEME(self, userInput):
+
+		path = os.path.realpath(__file__)
+
+		outputList = ["callMEME.py", userInput]
+
+		path = path.replace(outputList[0], outputList[1])
+	
+		command = "cd /sharing/students/MEME/bin/ ; ./meme "+path+" -dna -nmotifs 3 -minw 20 -maxw 50 -mod zoops -minsites 150 -maxsites 745"
+		subprocess.call(command, shell=True)
+
+		commandCopy = "cp -r /sharing/students/MEME/bin/meme_out" " "+path.replace(outputList[1],"")	
+		subprocess.call(commandCopy, shell=True)
+	
+		os.rename("meme_out", "output"+"("+outputList[1]+")")
