c9bb4fffb3100899fb3de4a79423e37aa44c9812
Roel_Beumers <roel_beumers@hotmail.com>
2014-05-30 02:26:06 +0200

    pipeline commit

--- callMEME.py
+++ callMEME.py
@@ -2,18 +2,18 @@ import os
 import subprocess
 
 class class4():
-    def callMEME(self, userInput):
+    def callMEME(self, userInput, Parameters):
 
-		path = os.path.realpath(__file__)
+	path = os.path.realpath(__file__)
 
-		outputList = ["callMEME.py", userInput]
+	outputList = ["callMEME.py", userInput]
 
-		path = path.replace(outputList[0], outputList[1])
+	path = path.replace(outputList[0], outputList[1])
 	
-		command = "cd /sharing/students/MEME/bin/ ; ./meme "+path+" -dna -nmotifs 3 -minw 20 -maxw 50 -mod zoops -minsites 150 -maxsites 745"
-		subprocess.call(command, shell=True)
+	command = "cd /sharing/students/MEME/bin/ ; ./meme "+path+" "+Parameters
+	subprocess.call(command, shell=True)
 
-		commandCopy = "cp -r /sharing/students/MEME/bin/meme_out" " "+path.replace(outputList[1],"")	
-		subprocess.call(commandCopy, shell=True)
+	commandCopy = "cp -r /sharing/students/MEME/bin/meme_out" " "+path.replace(outputList[1],"")	
+	subprocess.call(commandCopy, shell=True)
 	
-		os.rename("meme_out", "output"+"("+outputList[1]+")")
+	os.rename("meme_out", "output"+"("+outputList[1]+")")
